9edc5e21c8610f12de2c27b02da3b6b453b98971
Kerpal <luci3330@mylaurier.ca>
2019-11-10 22:33:05 -0500

    Finished Assignment3Part2

---
+++ app/src/main/java/com/example/stefan/androidassignments/WeatherForecast.java
@@ -0,0 +1,14 @@
+package com.example.stefan.androidassignments;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class WeatherForecast extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather_forecast);
+    }
+
+}
